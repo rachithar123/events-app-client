41093679b426e11e7aedbd61e8ff0250c76de47d
Rachitha Rubasinghe <rubasr01@myunitec.ac.nz>
2020-10-24 23:54:33 +1300

    update the config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
-    s3: {
-      REGION: "ap-southeast-2",
-      BUCKET: "events-manual"
-    },
-    apiGateway: {
-      REGION: "ap-southeast-2",
-      URL: "https://izn2og7zcl.execute-api.ap-southeast-2.amazonaws.com/prod/"
-    },
-    cognito: {
-      REGION: "ap-southeast-2",
-      USER_POOL_ID: "ap-southeast-2_TjoGOR35k",
-      APP_CLIENT_ID: "4qb5ek197gs6t2kdoqr3taee83",
-      IDENTITY_POOL_ID: "ap-southeast-2:02d6b0bf-2805-405f-9bc8-ec5ffb081646"
-    }
-  };
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "events-manual"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://f5zpdqztb3.execute-api.ap-southeast-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_TjoGOR35k",
+    APP_CLIENT_ID: "568pg6c9ghkhd1aibfsc5urkj7",
+    IDENTITY_POOL_ID: "ap-southeast-2:24c1bdaf-00bf-45ac-aa7f-0cabc8498065"
+  }
+};
